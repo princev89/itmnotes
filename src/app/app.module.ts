7848ff31c77943ce947678ead4ac5591b9cef2b2
princev89 <vishwakarmaprince833@gmail.com>
2021-05-11 13:07:26 +0530

    3 files modified, 9 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [  RouterModule.forRoot([
+      { path: 'login', component: LoginComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'home', component: HomeComponent },
+      {path: '**', component: LoginComponent}
+    ]),BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent, NavbarComponent, LoginComponent, HomeComponent, AboutComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
